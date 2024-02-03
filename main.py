41eb703f7a14ea8541152ce4adc2e981a1006aed
Marek Zalecki <mzalecki.ignis@gmail.com>
2024-02-03 12:59:59 +0100

    add hello world flask app

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
+from app import app
 
-def add(number_a: int | float, number_b: int | float) -> int | float:
-    return number_a + number_b
-
-
-if __name__ == '__main__':
-    numbers: list[int] = [1, 2]
-    print('numberA:', numbers[0])
-    print('numberB:', numbers[1])
-    print(f'{numbers[0]} + {numbers[1]} = {add(numbers[0], numbers[1])}')
